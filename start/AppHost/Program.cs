24ff24e32d886a65330fae37057a61562f17f4fc
jcastillopino <jorge@latintop.com>
2025-04-01 20:50:21 +0000

    Enhance MyWeatherHub project configuration with API reference for services discovery. Added the configuration for exposing the ip

--- start/AppHost/Program.cs
+++ start/AppHost/Program.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.Api>("api");
 
-var web = builder.AddProject<Projects.MyWeatherHub>("myweatherhub");
+var web = builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
+    .WithReference(api)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
